5c9348bffd04eae717b492daec4e96a55049abc7
PEDRO ARMANDO ALEMAN MORADO <paleman79@gmail.com>
2024-01-19 14:36:00 -0600

    first commit

---
+++ controller/apicontroller.js
@@ -0,0 +1,13 @@
+const verificar = (req, res) => {
+    res.send("Verificado");
+}
+
+const recibir = (req, res) => {
+    res.send("Recibido");
+}
+
+module.exports = {
+    verificar,
+    recibir
+}
+
